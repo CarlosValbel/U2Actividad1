da464c039f964a3c37e5c2505cab6cf6e56a25cb
krlos <krlos@ALBERTO-PC>
2023-10-03 22:06:26 -0600

    Agregar archivos de proyecto.

---
+++ U2Actividad1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using U2Actividad1.Models.ViewModels;
+
+namespace U2Actividad1.Controllers
+{
+    public class HomeController : Controller
+    {
+        U2Actividad1.Models.Entities.AnimalesContext context = new();
+        [Route("/")]
+        [Route("/Principal")]
+        public IActionResult Index()
+        {
+            var clases = context.Clases.OrderBy(x => x.Nombre);
+            return View(clases);
+        }
+        [Route("/{id}")]
+        public IActionResult Clase(string Id)
+        {
+            var especies = context.Especies
+                .Include(x => x.IdClaseNavigation)
+                .Where(x => x.IdClaseNavigation.Nombre == Id)
+                .OrderBy(x => x.Especie)
+                .Select(x => new ClaseViewModel { Id = x.Id, Nombre = x.Especie, Clase = x.IdClaseNavigation.Nombre });
+
+            if (especies.Count() > 0)
+            {
+                return View(especies);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [Route("/{clase}/{id}")]
+        public IActionResult Especie(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return RedirectToAction("Index");
+            }
+
+            id = id.Replace("-", " ");
+            var especie = context.Especies.Include(x => x.IdClaseNavigation)
+                .FirstOrDefault(x => x.Especie == id);
+
+            if (especie == null)
+                return RedirectToAction("Index");
+            return View(especie);
+        }
+    }
+}
---
+++ U2Actividad1/Models/ViewModels/ClaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace U2Actividad1.Models.ViewModels
+{
+    public class ClaseViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Clase { get; set; } = "";
+    }
+}
